839a0e9b8d204b0843d998bd94a8564f12d8c28e
Anna Freiman <annafrei924@gmail.com>
2024-02-20 17:58:44 -0500

    still fixing checkstyle

--- src/main/java/freiman/projectile/SimpleDocumentListener.java
+++ src/main/java/freiman/projectile/SimpleDocumentListener.java
@@ -3,16 +3,19 @@ package freiman.projectile;
 import javax.swing.event.*;
 
 public interface SimpleDocumentListener extends DocumentListener {
+
     void update(DocumentEvent e);
 
     @Override
     default void insertUpdate(DocumentEvent e) {
         update(e);
     }
+
     @Override
     default void removeUpdate(DocumentEvent e) {
         update(e);
     }
+
     @Override
     default void changedUpdate(DocumentEvent e) {
         update(e);
